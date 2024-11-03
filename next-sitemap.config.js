b904fa7921f0ab995432801000fae39d5156601e
Kane <117307814+kaneAmiller@users.noreply.github.com>
2024-11-03 08:51:04 -0500

    Delete next-sitemap.config.js

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://www.kmprojectsolutions.com',  
-    generateRobotsTxt: false, 
-    sitemapSize: 7000,
-  };
-  
